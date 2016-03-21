507305ae09a31b34513fd44d57257dab983d170b
Infatum <qwertty459@gmail.com>
2016-03-21 03:09:24 +0200

    finally works

--- Laba2/Encrypted.cs
+++ Laba2/Encrypted.cs
@@ -10,27 +10,68 @@ namespace Laba2
 {
     public class Encrypted
     {
-        private string OriginalText { get; set; }
-        public string EncryptedText { get; }
-        private string Password { get; set; }
-
-        public Encrypted(string text)
+        private Dictionary<char, int> alphabet = new Dictionary<char, int>()
         {
-            OriginalText = text;
+            {'А', 1}, {'Б', 2}, {'В', 3}, {'Г', 4}, {'Д', 5}, {'Е', 6}, {'Ё', 7}, {'Ж', 8}, {'З', 9}, {'И', 10}, {'Й', 11}, {'К', 12}, {'Л', 13}, {'М', 14}, {'Н', 15}, {'О', 16}, {'П', 17}, {'Р', 18}, {'С', 19}, {'Т', 20}, {'У', 21}, {'Ф', 22}, {'Х', 23}, {'Ц', 24}, {'Ч', 25}, {'Ш', 26}, {'Щ', 27},{'Ы', 28}, {'Ь', 29}, {'Э', 30}, {'Ю', 31}, {'Я', 32},
+            {'а', 33}, {'б', 34}, {'в', 35}, {'г', 36}, {'д', 37}, {'е', 38}, {'ё', 39}, {'ж', 40}, {'з', 41}, {'и', 42}, {'й', 43}, {'к', 44}, {'л', 45}, {'м', 46}, {'н', 47}, {'о', 48}, {'п', 49}, {'р', 50}, {'с', 51}, {'т', 52}, {'у', 53}, {'ф', 54}, {'х', 55}, {'ц', 56}, {'ч', 57}, {'ш', 58}, {'щ', 59},{'ы', 60}, {'ь', 61}, {'э', 62}, {'ю', 63}, {'я', 64},
+            {'A', 65}, {'B', 66}, {'C', 67}, {'D', 68}, {'E', 69}, {'F', 70}, {'G', 71}, {'H', 72}, {'I', 73}, {'J', 74}, {'K', 75}, {'L', 76}, {'M', 77}, {'N', 78}, {'O', 79}, {'P', 80}, {'Q', 81}, {'R', 82}, {'S', 83}, {'T', 84}, {'U', 85}, {'V', 86}, {'W', 87}, {'X', 88}, {'Y', 89}, {'Z', 90},
+            {'a', 91}, {'b', 92}, {'c', 93}, {'d', 94}, {'e', 95}, {'f', 96}, {'g', 97}, {'h', 98}, {'i', 99}, {'j', 100}, {'k', 101}, {'l', 102}, {'m', 103}, {'n', 104}, {'o', 105}, {'p', 106}, {'q', 107}, {'r', 108}, {'s', 109}, {'t', 110}, {'u', 111}, {'v', 112}, {'w', 113}, {'x', 114}, {'y', 115}, {'z', 116},
+        };
+        private string OriginalText { get; set; }
+        public string EncryptedText { get; set; }
+        private string Password { get; set; }
+        private string SourcePath { get; set; }
+        private string EncryptedFilePath { get; set; }
+        private string PasswordFilePath { get; set; }
+        public string DecryptedPath { get; set; }
+
+        public Encrypted(string sourcePath)
+        {
+            SourcePath = sourcePath;
         }
 
-        public async void CreateFilesWithText(string path, string path2, Encoding encoding)
+        public Encrypted(string text, string sourcePath)
+        {
+            OriginalText = text;
+            SourcePath = sourcePath;
+        }
+
+        public Encrypted(string text, string sourcePath, string secreeKey)
+        {
+            OriginalText = text;
+            SourcePath = sourcePath;
+            Password = secreeKey;
+        }
+
+        public void CreateEmptyFiles(string originalFilePath, string encryptedFilePath, string passwordFilePath)
         {
             try
             {
-                FileStream encryptedFile = new FileStream(path2, FileMode.CreateNew);
-                UnicodeEncoding uniencoding = new UnicodeEncoding();
-                byte[] result = uniencoding.GetBytes(OriginalText);
+                FileStream originalFile = new FileStream(originalFilePath, FileMode.CreateNew);
+                FileStream encryptedFile = new FileStream(encryptedFilePath, FileMode.CreateNew);
+                FileStream passwordFile = new FileStream(passwordFilePath, FileMode.CreateNew);
+                originalFile.Dispose();
+                encryptedFile.Dispose();
+                passwordFile.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
-                using (FileStream source = File.Open(path, FileMode.OpenOrCreate))
+        public async void WriteTextToFiles(string originalFilePath, string originalText)
+        {
+            try
+            {
+                SourcePath = originalFilePath;
+                UnicodeEncoding uniencoding = new UnicodeEncoding();
+                byte[] result = uniencoding.GetBytes(originalText);
+                char[] text = Encoding.Unicode.GetChars(result);
+
+                using (StreamWriter wr = new StreamWriter(originalFilePath, false))
                 {
-                    source.Seek(0, SeekOrigin.End);
-                    await source.WriteAsync(result, 0, result.Length);
+                    await wr.WriteAsync(originalText);
                 }
             }
             catch (Exception ex)
@@ -39,23 +80,25 @@ namespace Laba2
             }
         }
 
-        public void WritePasswordToFile(string password, string path)
+        public async void WritePasswordToFile(string password, string passwordPath)
         {
             Password = password;
+            PasswordFilePath = password;
             UnicodeEncoding uniencoding = new UnicodeEncoding();
-            byte[] result = uniencoding.GetBytes(password);
+            byte[] result = uniencoding.GetBytes(Password);
+            char[] paswd = Encoding.Unicode.GetChars(result);
 
-            using (FileStream wr = new FileStream(path, FileMode.Open))
+            using (StreamWriter wr = new StreamWriter(passwordPath, false))
             {
-                wr.Write(result, 0, result.Length);
+                await wr.WriteAsync(paswd);
             }
         }
 
-        public async Task<int> ReadTextFileByteCount(string path)
+        public async Task<int> OriginalTextFileByteCount(string originalFilePath)
         {
             int asciSymb = 0;
 
-            using (StreamReader sr = new StreamReader(path))
+            using (StreamReader sr = new StreamReader(originalFilePath))
             {
                 var s = sr.ReadToEnd();
                 Task<int> ts = sr.ReadAsync(s.ToCharArray(), 0, s.Length);
@@ -89,65 +132,116 @@ namespace Laba2
             return asciSymb;
         }
 
-        public async Task<byte[]> ReadPasswordFileToByteArray(string path)
+        public byte[] ReadPasswordFileToByteArray(string path)
         {
             byte[] result;
             using (FileStream fileSource = File.Open(path, FileMode.Open))
             {
                 result = new byte[fileSource.Length];
-                await fileSource.ReadAsync(result, 0, (int)fileSource.Length);   
+                fileSource.ReadAsync(result, 0, (int)fileSource.Length);
             }
             return result;
         }
 
-        public async Task<byte[]> EncryptTextFile(string sourceFilePath, string encryptedFilePath)
+        public async void EncryptTextFile(string passwordFilePath, string encryptedFilePath)
         {
-            byte[] password = ReadPasswordFileToByteArray(sourceFilePath).Result;
-            byte[] encryptedText;
+            UnicodeEncoding uniencoding = new UnicodeEncoding();
+            StringBuilder encryptedText = new StringBuilder();
+            byte[] originalTextBytes = uniencoding.GetBytes(OriginalText);
+            char[] originalTextChars = Encoding.Unicode.GetChars(originalTextBytes);
+            char[] encryptedChars = new char[originalTextChars.Length];
+            byte[] result = uniencoding.GetBytes(Password);
+            char[] paswd = Encoding.Unicode.GetChars(result);
             int index = 0;
+            int keyCarret = 0;
+            int tmpCharNumber = -1;
+            int tmpKeyCharNumber = -1;
+            int tmpCryptedCharNumber = -1;
 
-            using (FileStream fileSource = File.Open(sourceFilePath, FileMode.Open))
+            foreach (var c in originalTextChars)
             {
-                encryptedText = new byte[fileSource.Length];
-                for (int i = 0; i < fileSource.Length; i++)
+                if (alphabet.ContainsKey(c))
                 {
-                    
-                    if(i == password.Length - 1)
+                    tmpCharNumber = alphabet[c]; //Gets the current index of a character
+                    tmpKeyCharNumber = alphabet[paswd[keyCarret % paswd.Length]];//Get index of current password character
+                    tmpCryptedCharNumber = (tmpCharNumber + tmpKeyCharNumber);
+
+                    if (tmpCryptedCharNumber > alphabet.Count)
                     {
-                        index = 0;
+                        tmpCryptedCharNumber -= alphabet.Count;//Encrupt current char in text with character from password
+
                     }
-                    
-                    encryptedText[i] += (byte)(password[index] % ((int)'А' - 1));
-                    ++index;
+                    encryptedChars[index] = GetKeyByValue(tmpCryptedCharNumber);
                 }
-                using (FileStream encryptedFile = File.OpenWrite(encryptedFilePath))
+                else
                 {
-                    encryptedFile.Seek(0, SeekOrigin.Begin);
-                    await encryptedFile.WriteAsync(encryptedText, 0, encryptedText.Length);
+                    encryptedChars[index] = c;
                 }
+                ++index;
             }
-            return encryptedText;
+            using (StreamWriter streamCrypted = new StreamWriter(encryptedFilePath))
+            {
+                await streamCrypted.WriteAsync(encryptedChars);
+            }
+            encryptedText.Append(encryptedChars);
+            this.EncryptedText = encryptedText.ToString();
         }
 
-        public byte[] DecryptTextFile(string encryptedFilePath, string passwordFilePath)
+        private char GetKeyByValue(int value)
         {
-            byte[] decryptedText;
-            byte[] encryptedText;
-            int passwordCount = 
-            int index = 0;
-
-            using (FileStream encrypted = File.OpenRead(encryptedFilePath))
+            char escape = ' ';
+            foreach (var chr in this.alphabet)
             {
-                encryptedText = new byte[encrypted.Length];
-                encrypted.Read(encryptedText, 0, (int)encrypted.Length);
-
-                for (int i = 0; i < encrypted.Length; i++)
+                if (chr.Value == value)
                 {
-                    encryptedText[i] += (byte)(password[index] % ((int)'А' - 1));
-                    ++index;
+                    return chr.Key;
                 }
+            }
+            return escape;
+        }
 
-            }  
+        public string DecryptTextFile(string encryptedFilePath, string decryptedFilePath)
+        {
+            UnicodeEncoding uniencoding = new UnicodeEncoding();
+            StringBuilder decryptedText = new StringBuilder();
+            byte[] encryptedTextBytes = uniencoding.GetBytes(EncryptedText);
+            char[] encryptedTextChars = Encoding.Unicode.GetChars(encryptedTextBytes);
+            char[] decryptedChars = new char[encryptedTextChars.Length];
+            byte[] result = uniencoding.GetBytes(Password);
+            char[] paswd = Encoding.Unicode.GetChars(result);
+            int index = 0;
+            int keyCarret = 0;
+            int tmpCharNumber = -1;
+            int tmpKeyCharNumber = -1;
+            int tmpCryptedCharNumber = -1;
+
+            foreach (var c in encryptedTextChars)
+            {
+                if (alphabet.ContainsKey(c))
+                {
+                    tmpCharNumber = alphabet[c]; //Gets the current index of a character
+                    tmpKeyCharNumber = alphabet[paswd[keyCarret % paswd.Length]];//Get index of current password character
+                    tmpCryptedCharNumber = (tmpCharNumber - tmpKeyCharNumber);
+
+                    if (tmpCryptedCharNumber < 1)
+                    {
+                        tmpCryptedCharNumber += alphabet.Count;//Encrupt current char in text with character from password
+
+                    }
+                    decryptedChars[index] = GetKeyByValue(tmpCryptedCharNumber);
+                }
+                else
+                {
+                    decryptedChars[index] = c;
+                }
+                ++index;
+            }
+
+            using (StreamWriter streamCrypted = new StreamWriter(encryptedFilePath))
+            {
+                 streamCrypted.Write(decryptedChars);
+            }
+            return decryptedText.Append(decryptedChars).ToString();
         }
     }
 }
--- Laba2/MainWindow.xaml.cs
+++ Laba2/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.IO;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,9 +21,91 @@ namespace Laba2
     /// </summary>
     public partial class MainWindow : Window
     {
+        Encrypted file;
+        UnicodeEncoding encoding;
+        string directoryPath;
+        string filename;
+        string passwordfileName;
+        string encryptedFileName;
+        string DecryptedFileName;
+
         public MainWindow()
         {
             InitializeComponent();
+            if (String.IsNullOrEmpty(fileName.Text))
+            {
+                CreateFile_btn.IsEnabled = false;
+            }
+        }
+
+        private void CreateFile_btn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void NameChanged(object sender, TextChangedEventArgs e)
+        {
+            CreateFile_btn.IsEnabled = true;
+        }
+
+        private void Create_File(object sender, RoutedEventArgs e)
+        {
+
+            CreateFiles();
+        }
+
+        private void CreateFiles()
+        {
+            directoryPath = "./Laba3";
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            filename = directoryPath + "/" + fileName.Text + ".txt";
+            encryptedFileName = directoryPath + '/' + fileName.Text + "_Encrypted" + ".txt";
+            passwordfileName = directoryPath + '/' + fileName.Text + "_Password" + ".txt";
+            file = new Encrypted(fileContent.Text, filename);
+            file.CreateEmptyFiles(filename, encryptedFileName, passwordfileName);
+        }
+
+        private void Save_File_Click(object sender, RoutedEventArgs e)
+        {
+            if (filename != null)
+            {
+                file.WriteTextToFiles(filename, fileContent.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please, create file first");
+                return;
+            }
+        }
+
+        private void Encrypt_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (filename == null)
+            {
+                MessageBox.Show("Create or choose file first");
+                return;
+            }
+            file.WritePasswordToFile(this.Password_Text.Text, passwordfileName);
+            file.EncryptTextFile(passwordfileName, encryptedFileName);
+
+        }
+
+        private void Decrypt_Button_Click(object sender, RoutedEventArgs e)
+        {
+            DecryptedFileName = directoryPath + '/' + fileName.Text + "_Decrypted" + ".txt";
+            if (File.Exists(encryptedFileName))
+            {
+                if (!File.Exists(DecryptedFileName))
+                {
+                    FileStream decryptedFile = new FileStream(DecryptedFileName, FileMode.CreateNew);
+                    decryptedFile.Dispose();
+                }
+                Encrypted_Decrypted_TextBox.Text = file.DecryptTextFile(encryptedFileName, DecryptedFileName);
+            }
         }
     }
 }
